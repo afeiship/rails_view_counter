9e85b68ef7987a9b5a43a023117a413e82697e0b
aric.zheng <1290657123@qq.com>
2025-11-07 09:40:19 +0800

    fix: missing generators when install

--- lib/generators/rails_view_counter/migration_generator.rb
+++ lib/generators/rails_view_counter/migration_generator.rb
@@ -5,17 +5,21 @@ module RailsViewCounter
     source_root File.expand_path('../templates', __dir__)
     desc 'Generates migration for adding view counter to a model'
 
-    argument :table_name, type: :string, desc: 'Table name to add view counter to'
+    argument :table_name, type: :string, required: true, desc: 'Table name to add view counter to'
 
     def create_migration_file
-      migration_template 'migration.rb', 
-                        "db/migrate/add_view_counter_to_#{table_name}.rb"
+      migration_file = File.join("db/migrate", "#{timestamp}_add_view_counter_to_#{table_name.singularize}.rb")
+      template 'migration.rb', migration_file
     end
 
     private
 
     def source_paths
       [File.expand_path('../templates', __dir__)]
+    end
+
+    def timestamp
+      Time.current.strftime("%Y%m%d%H%M%S")
     end
   end
 end
