aad23da714c0abb50c1f10e0f2d0b94188dd8cdb
aric.zheng <1290657123@qq.com>
2025-11-07 09:31:17 +0800

    fix: missing generators when install

--- lib/generators/rails_view_counter/install_generator.rb
+++ lib/generators/rails_view_counter/install_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators'
 
 module RailsViewCounter
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../templates', __dir__)
+    source_root File.expand_path('templates', __dir__)
     desc 'Generates Rails View Counter initializer'
 
     def copy_initializer
